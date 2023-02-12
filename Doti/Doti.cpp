5dbd432e068c608a66ffa183eb4e0ae9d73ff166
Thomas Pany <thomas.pany@unibw.de>
2023-02-12 09:58:17 +0100

    update for laptop 10875H results

--- Doti/Doti.cpp
+++ Doti/Doti.cpp
@@ -9,6 +9,7 @@
 
 
 
+
 // InstructionSet.cpp
 // Compile by using: cl /EHsc /W4 InstructionSet.cpp
 // processor: x86, x64
@@ -208,25 +209,24 @@ const InstructionSet::InstructionSet_Internal InstructionSet::CPU_Rep;
 
 int main()
 {
-    //auto& outstream = std::cout;
+    auto& outstream = std::cout;
 
-    //auto support_message = [&outstream](std::string isa_feature, bool is_supported) {
-    //    outstream << isa_feature << (is_supported ? " supported" : " not supported") << std::endl;
-    //};
+    auto support_message = [&outstream](std::string isa_feature, bool is_supported) {
+        outstream << isa_feature << (is_supported ? " supported" : " not supported") << std::endl;
+    };
 
-    //std::cout << InstructionSet::Vendor() << std::endl;
-    //std::cout << InstructionSet::Brand() << std::endl;
+    std::cout << InstructionSet::Vendor() << std::endl;
+    std::cout << InstructionSet::Brand() << std::endl;
 
-    //support_message("AVX", InstructionSet::AVX());
-    //support_message("AVX2", InstructionSet::AVX2());
-    //support_message("AVX512CD", InstructionSet::AVX512CD());
-    //support_message("AVX512ER", InstructionSet::AVX512ER());
-    //support_message("AVX512F", InstructionSet::AVX512F());
-    //support_message("AVX512PF", InstructionSet::AVX512PF());
-    //support_message("FMA", InstructionSet::FMA());
-    //support_message("PCLMULQDQ", InstructionSet::PCLMULQDQ());
-    //support_message("POPCNT", InstructionSet::POPCNT());
-
+    support_message("AVX", InstructionSet::AVX());
+    support_message("AVX2", InstructionSet::AVX2());
+    support_message("AVX512CD", InstructionSet::AVX512CD());
+    support_message("AVX512ER", InstructionSet::AVX512ER());
+    support_message("AVX512F", InstructionSet::AVX512F());
+    support_message("AVX512PF", InstructionSet::AVX512PF());
+    support_message("FMA", InstructionSet::FMA());
+    support_message("PCLMULQDQ", InstructionSet::PCLMULQDQ());
+    support_message("POPCNT", InstructionSet::POPCNT());
 
 
 
@@ -238,89 +238,26 @@ int main()
     ippInit();
     /* Get IPP library version info */
     lib = ippGetLibVersion();
-    printf("%s %s\n", lib->Name, lib->Version);
+    std::cout  << "\n\n\n" << lib->Name << "/" << lib->Version;
 
     /* Get CPU features and features enabled with selected library level */
     status = ippGetCpuFeatures(&mask, 0);
     if (ippStsNoErr == status) {
         emask = ippGetEnabledCpuFeatures();
-        printf("Features supported by CPU\tby IPP\n");
-        printf("-----------------------------------------\n");
-        printf("  ippCPUID_MMX        = ");
-        printf("%c\t%c\t", (mask & ippCPUID_MMX) ? 'Y' : 'N', (emask & ippCPUID_MMX) ? 'Y' : 'N');
-        printf("Intel(R) Architecture MMX technology supported\n");
-        printf("  ippCPUID_SSE        = ");
-        printf("%c\t%c\t", (mask & ippCPUID_SSE) ? 'Y' : 'N', (emask & ippCPUID_SSE) ? 'Y' : 'N');
-        printf("Intel(R) Streaming SIMD Extensions\n");
-        printf("  ippCPUID_SSE2       = ");
-        printf("%c\t%c\t", (mask & ippCPUID_SSE2) ? 'Y' : 'N', (emask & ippCPUID_SSE2) ? 'Y' : 'N');
-        printf("Intel(R) Streaming SIMD Extensions 2\n");
-        printf("  ippCPUID_SSE3       = ");
-        printf("%c\t%c\t", (mask & ippCPUID_SSE3) ? 'Y' : 'N', (emask & ippCPUID_SSE3) ? 'Y' : 'N');
-        printf("Intel(R) Streaming SIMD Extensions 3\n");
-        printf("  ippCPUID_SSSE3      = ");
-        printf("%c\t%c\t", (mask & ippCPUID_SSSE3) ? 'Y' : 'N', (emask & ippCPUID_SSSE3) ? 'Y' : 'N');
-        printf("Intel(R) Supplemental Streaming SIMD Extensions 3\n");
-        printf("  ippCPUID_MOVBE      = ");
-        printf("%c\t%c\t", (mask & ippCPUID_MOVBE) ? 'Y' : 'N', (emask & ippCPUID_MOVBE) ? 'Y' : 'N');
-        printf("The processor supports MOVBE instruction\n");
-        printf("  ippCPUID_SSE41      = ");
-        printf("%c\t%c\t", (mask & ippCPUID_SSE41) ? 'Y' : 'N', (emask & ippCPUID_SSE41) ? 'Y' : 'N');
-        printf("Intel(R) Streaming SIMD Extensions 4.1\n");
-        printf("  ippCPUID_SSE42      = ");
-        printf("%c\t%c\t", (mask & ippCPUID_SSE42) ? 'Y' : 'N', (emask & ippCPUID_SSE42) ? 'Y' : 'N');
-        printf("Intel(R) Streaming SIMD Extensions 4.2\n");
-        printf("  ippCPUID_AVX        = ");
-        printf("%c\t%c\t", (mask & ippCPUID_AVX) ? 'Y' : 'N', (emask & ippCPUID_AVX) ? 'Y' : 'N');
-        printf("Intel(R) Advanced Vector Extensions instruction set\n");
-        printf("  ippAVX_ENABLEDBYOS  = ");
-        printf("%c\t%c\t", (mask & ippAVX_ENABLEDBYOS) ? 'Y' : 'N', (emask & ippAVX_ENABLEDBYOS) ? 'Y' : 'N');
-        printf("The operating system supports Intel(R) AVX\n");
-        printf("  ippCPUID_AES        = ");
-        printf("%c\t%c\t", (mask & ippCPUID_AES) ? 'Y' : 'N', (emask & ippCPUID_AES) ? 'Y' : 'N');
-        printf("Intel(R) AES instruction\n");
-        printf("  ippCPUID_SHA        = ");
-        printf("%c\t%c\t", (mask & ippCPUID_SHA) ? 'Y' : 'N', (emask & ippCPUID_SHA) ? 'Y' : 'N');
-        printf("Intel(R) SHA new instructions\n");
-        printf("  ippCPUID_CLMUL      = ");
-        printf("%c\t%c\t", (mask & ippCPUID_CLMUL) ? 'Y' : 'N', (emask & ippCPUID_CLMUL) ? 'Y' : 'N');
-        printf("PCLMULQDQ instruction\n");
-        printf("  ippCPUID_RDRAND     = ");
-        printf("%c\t%c\t", (mask & ippCPUID_RDRAND) ? 'Y' : 'N', (emask & ippCPUID_RDRAND) ? 'Y' : 'N');
-        printf("Read Random Number instructions\n");
-        printf("  ippCPUID_F16C       = ");
-        printf("%c\t%c\t", (mask & ippCPUID_F16C) ? 'Y' : 'N', (emask & ippCPUID_F16C) ? 'Y' : 'N');
-        printf("Float16 instructions\n");
-        printf("  ippCPUID_AVX2       = ");
-        printf("%c\t%c\t", (mask & ippCPUID_AVX2) ? 'Y' : 'N', (emask & ippCPUID_AVX2) ? 'Y' : 'N');
-        printf("Intel(R) Advanced Vector Extensions 2 instruction set\n");
-        printf("  ippCPUID_AVX512F    = ");
-        printf("%c\t%c\t", (mask & ippCPUID_AVX512F) ? 'Y' : 'N', (emask & ippCPUID_AVX512F) ? 'Y' : 'N');
-        printf("Intel(R) Advanced Vector Extensions 3.1 instruction set\n");
-        printf("  ippCPUID_AVX512CD   = ");
-        printf("%c\t%c\t", (mask & ippCPUID_AVX512CD) ? 'Y' : 'N', (emask & ippCPUID_AVX512CD) ? 'Y' : 'N');
-        printf("Intel(R) Advanced Vector Extensions CD (Conflict Detection) instruction set\n");
-        printf("  ippCPUID_AVX512ER   = ");
-        printf("%c\t%c\t", (mask & ippCPUID_AVX512ER) ? 'Y' : 'N', (emask & ippCPUID_AVX512ER) ? 'Y' : 'N');
-        printf("Intel(R) Advanced Vector Extensions ER instruction set\n");
-        printf("  ippCPUID_ADCOX      = ");
-        printf("%c\t%c\t", (mask & ippCPUID_ADCOX) ? 'Y' : 'N', (emask & ippCPUID_ADCOX) ? 'Y' : 'N');
-        printf("ADCX and ADOX instructions\n");
-        printf("  ippCPUID_RDSEED     = ");
-        printf("%c\t%c\t", (mask & ippCPUID_RDSEED) ? 'Y' : 'N', (emask & ippCPUID_RDSEED) ? 'Y' : 'N');
-        printf("The RDSEED instruction\n");
-        printf("  ippCPUID_PREFETCHW  = ");
-        printf("%c\t%c\t", (mask & ippCPUID_PREFETCHW) ? 'Y' : 'N', (emask & ippCPUID_PREFETCHW) ? 'Y' : 'N');
-        printf("The PREFETCHW instruction\n");
-        printf("  ippCPUID_KNC        = ");
-        printf("%c\t%c\t", (mask & ippCPUID_KNC) ? 'Y' : 'N', (emask & ippCPUID_KNC) ? 'Y' : 'N');
-        printf("Intel(R) Xeon Phi(TM) Coprocessor instruction set\n");
+        std::cout << "\nFeatures supported by CPU\tby IPP\n";
+        std::cout << "-----------------------------------------\n";
+        std::cout << "Intel(R) Advanced Vector Extensions 2 instruction set\n";
+        std::cout << "  ippCPUID_AVX512F    = " << ((mask & ippCPUID_AVX512F) ? 'Y' : 'N') << "/" << ((emask & ippCPUID_AVX512F) ? 'Y' : 'N') << "\n";
+        std::cout << "Intel(R) Advanced Vector Extensions 3.1 instruction set\n";
+        std::cout << "  ippCPUID_AVX512CD   = " << ((mask & ippCPUID_AVX512CD) ? "Y" : "N" ) << "/" << ( (emask & ippCPUID_AVX512CD) ? 'Y' : 'N' );
+        std::cout << "\n\n";
     }
 
 
 
 	__int16* x,*y ;
 	int lenmax = 100000;
+    int runs = 1000000;
 	x = (__int16*)malloc((lenmax+000) * sizeof(__int16));
 	y = (__int16*)malloc((lenmax+000) * sizeof(__int16));
 	
@@ -332,9 +269,11 @@ int main()
 		y[k] = k % 19;
 	}
 	
+    // VAR1
+
 	__int64 ops = 0;
 	__int64 sum = 0;
-	for (unsigned __int32 l = 0; l < 100000; l++)
+	for (unsigned __int32 l = 0; l < runs; l++)
 	{
         unsigned int sx = (l * 234) % lenmax;
         unsigned int sy = (l * 2324) % lenmax;
@@ -344,20 +283,43 @@ int main()
 		int len = std::min(lenmax - sx, lenmax - sy);
 		ops += 2*len;
 		zs = 0;
-        // VAR 1
-        //ippsDotProd_16s32s_Sfs(x + sx, y + sy, len, &zs, 0);
-
-        // VAR 2
-		for (int n = 0; n < len; n++)
-		{
-			zs = zs + x[sx + n] * y[sy + n];
-		}
-		sum = sum + __int64(zs);
+        ippsDotProd_16s32s_Sfs(x + sx, y + sy, len, &zs, 0);
+        sum = sum + zs;
 	}
 
 	auto ende = std::chrono::high_resolution_clock::now();
 	int usec =  std::chrono::duration_cast<std::chrono::microseconds>(ende - start).count();
+    std::cout << "ippsDotProd_16s32s_Sfs: Sum:" << sum << "; OPS=" << ops  << "; Time [us]=" << usec << "; GOPS=" << double(ops)/(double(usec)*1e-6)/1e9 << "\n";
 
-	std::cout << "Summme:" << sum << "; OPS=" << ops  << "; Zeit=" << usec << "; GOPS=" << double(ops)/(double(usec)*1e-6)/1e9 << "\n";
+
+
+    // VAR2
+     ops = 0;
+     sum = 0;
+    for (unsigned __int32 l = 0; l < runs; l++)
+    {
+        unsigned int sx = (l * 234) % lenmax;
+        unsigned int sy = (l * 2324) % lenmax;
+
+        Ipp32s zs;
+
+        int len = std::min(lenmax - sx, lenmax - sy);
+        ops += 2 * len;
+        zs = 0;
+
+
+        for (int n = 0; n < len; n++)
+        {
+        	zs = zs + x[sx + n] * y[sy + n];
+        }
+        sum = sum + __int64(zs);
+    }
+
+     ende = std::chrono::high_resolution_clock::now();
+     usec = std::chrono::duration_cast<std::chrono::microseconds>(ende - start).count();
+
+    std::cout << "C++ loop: Sum=" << sum << "; OPS=" << ops << "; Time [us]=" << usec << "; GOPS=" << double(ops) / (double(usec) * 1e-6) / 1e9 << "\n";
+
+
 }
 
--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# DotProduct
-test
+# DotProduct  Results
+
+#Intel(R) Core(TM) i7-10875H CPU @ 2.30GHz
+AVX supported
+AVX2 supported
+AVX512CD not supported
+AVX512ER not supported
+AVX512F not supported
+AVX512PF not supported
+FMA supported
+PCLMULQDQ supported
+POPCNT supported
+
+
+
+ippCore/2021.7 (r0xa954907f)
+Features supported by CPU       by IPP
+-----------------------------------------
+Intel(R) Advanced Vector Extensions 2 instruction set
+  ippCPUID_AVX512F    = N/N
+Intel(R) Advanced Vector Extensions 3.1 instruction set
+  ippCPUID_AVX512CD   = N/N
+
+ippsDotProd_16s32s_Sfs: Sum:2400111292740; OPS=66670216800; Time [us]=3709172; GOPS=17.9744
+C++ loop: Sum=2400111292740; OPS=66670216800; Time [us]=5598353; GOPS=11.9089
+
